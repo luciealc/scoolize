8675646aaf60b03b2a163342378a84e225403194
luciealc <114408320+luciealc@users.noreply.github.com>
2023-12-04 18:39:27 +0100

    correction erreur init login & register

---
+++ src/page_prepare/auth/Register.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export interface IRegisterPageProps {};
+
+const RegisterPage: React.FunctionComponent<IRegisterPageProps>=(props)=>{
+    return (<div></div>)
+}
+
+export default RegisterPage
