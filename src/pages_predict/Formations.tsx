ffc20935e25565487557d80fa3d0ad2bcfe5abcd
luciealc <114408320+luciealc@users.noreply.github.com>
2023-12-10 22:31:21 +0100

    scroll container card formation

--- src/pages_predict/Formations.tsx
+++ src/pages_predict/Formations.tsx
@@ -8,6 +8,7 @@ import MapFormationPage from './components/MapFormation';
 export interface IFormationsPageProps { };
 
 const FormationsPage: React.FunctionComponent<IFormationsPageProps> = (props) => {
+
     return (
         <MainContentPage>
             <div className="container mx-auto p-4">
@@ -15,12 +16,17 @@ const FormationsPage: React.FunctionComponent<IFormationsPageProps> = (props) =>
                     <SearchBarPage />
                     <FormationFilterPage />
                 </div>
-                <div className="flex flex-wrap -mx-2">
+                <div className="flex flex-wrap mx-2">
                     <div className="w-full lg:w-1/2 px-2 mb-4">
                         <div className="w-[191px] h-[34px] text-black text-[32px] font-medium-marianne leading-tight">Proposition</div>
-                        <CardFormationPage />
+                        <div className='overflow-y-auto mt-2 mb-2 ml-2 mr-2 p-2 rounded-lg' style={{ maxHeight: '700px' }}>
+                            <CardFormationPage />
+                            <CardFormationPage />
+                            <CardFormationPage />
+                            <CardFormationPage />
+                        </div>
                     </div>
-                    <div className="w-full lg:w-1/2 px-2">
+                    <div className="w-full lg:w-1/2 px-2 h-full">
                         <div className="w-[191px] h-[34px] text-black text-[32px] font-medium-marianne leading-tight">Carte</div>
                         <MapFormationPage />
                     </div>
