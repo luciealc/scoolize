f0d2fac6a9a9e6cb16ce97293382d579e467d1cb
luciealc <114408320+luciealc@users.noreply.github.com>
2023-12-11 23:07:16 +0100

    liste de formation + popup de modif

--- src/page_prepare/components/ButtonFormation.tsx
+++ src/page_prepare/components/ButtonFormation.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
-export interface IButtonFormationPageProps {};
+interface IButtonFormationPageProps {}
 
-const ButtonFormationPage: React.FunctionComponent<IButtonFormationPageProps>=(props)=>{
-    return (<div>
+const ButtonFormationPage: React.FC<IButtonFormationPageProps> = (props) => {
+    return (
         <div className='w-[179px] h-[97px] px-8 py-6 bg-lightlight-blue border border-dark-blue justify-center items-center gap-2.5 inline-flex'>
-            <button className='text-center text-black text-xl font-medium-marianne leading-normal'>Ajouter une Formation</button>
+            <button className='text-center text-black text-xl font-medium-marianne leading-normal'>
+                Ajouter une Formation
+            </button>
         </div>
-    </div>)
-}
+    );
+};
 
-export default ButtonFormationPage
+export default ButtonFormationPage;
