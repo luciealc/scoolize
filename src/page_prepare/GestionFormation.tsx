8e7466d918da209469342953befd955a8f0eba3e
luciealc <114408320+luciealc@users.noreply.github.com>
2023-12-11 16:08:35 +0100

    search, filtre et bouton

--- src/page_prepare/GestionFormation.tsx
+++ src/page_prepare/GestionFormation.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import MainContentPage from './components/MainContent';
+import SearchBarPage from './components/SearchBar';
+import FormationFilterPage from './components/FilterFormation';
+import ButtonFormationPage from './components/ButtonFormation';
 
 export interface IGestionFormationPageProps {};
 
 const GestionFormationPage: React.FunctionComponent<IGestionFormationPageProps>=(props)=>{
     return (<div>
         <MainContentPage>
-            <h1>Gestion des Formations</h1>
+            <div className='container mx-auto p-4'>
+                <div className='flex flex-col md:flex-row justify-between items-start md:items-center mb-4 space-y-4 md:space-y-0'>
+                    <SearchBarPage />
+                    <FormationFilterPage />
+                    <ButtonFormationPage />
+                </div>
+            </div>
         </MainContentPage>
     </div>)
 }
