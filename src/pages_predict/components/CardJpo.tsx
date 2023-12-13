c6f04360fb4529a3525bad62e8efec862ce97d70
luciealc <114408320+luciealc@users.noreply.github.com>
2023-12-13 21:54:31 +0100

    issue fix?

---
+++ src/pages_predict/components/CardJpo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export interface ICardJpoPageProps {};
+
+const CardJpoPage: React.FunctionComponent<ICardJpoPageProps>=(props)=>{
+    return (<div>
+        <h1>Date JPO</h1>
+    </div>)
+}
+
+export default CardJpoPage
