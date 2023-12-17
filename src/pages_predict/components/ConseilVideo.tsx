935f5c96d6dc606df615552a8a1383e2f8e4e9a9
luciealc <114408320+luciealc@users.noreply.github.com>
2023-12-17 23:18:12 +0100

    orientation finie

--- src/pages_predict/components/ConseilVideo.tsx
+++ src/pages_predict/components/ConseilVideo.tsx
@@ -7,7 +7,7 @@ export interface IConseilVideoPageProps {
 const ConseilVideoPage: React.FunctionComponent<IConseilVideoPageProps> = ({ videoUrls }) => {
     return (
         <div>
-            <h2 className="text-[27px] font-medium-marianne mb-4">Conseils vidéo</h2>
+            <h2 className="text-[27px] font-medium-marianne mb-4 tracking-tight">Conseils vidéo</h2>
 
             <div className="grid grid-cols-2 gap-4 h-[445px] overflow-y-auto scrollbar-hide space-x-4">
                 {videoUrls.map((url, index) => (
--- src/pages_predict/components/PopupFicheMetier.tsx
+++ src/pages_predict/components/PopupFicheMetier.tsx
@@ -2,16 +2,22 @@ import React from 'react';
 
 export interface IPopupFicheMetierPageProps {
     onClose: () => void;
+    children: React.ReactNode
 };
 
-const PopupFicheMetierPage: React.FunctionComponent<IPopupFicheMetierPageProps>=({onClose})=>{
+const PopupFicheMetierPage: React.FunctionComponent<IPopupFicheMetierPageProps>=({onClose, children})=>{
     return (<div>
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-            <div className="bg-white p-4 rounded">
-                <h2>Popup Content Here</h2>
-                <button onClick={onClose}>Close</button>
+        <div className="fixed inset-0 overflow-y-auto h-full w-full z-50 flex justify-center items-start pt-20">
+                {/* Popup width of 4/6 centered to the right */}
+                <div className="w-4/6 ml-auto p-5 border-dark-blue shadow-lg bg-white mr-[90px]">
+                    {/* Popup content */}
+                    <div className="flex justify-between items-center pb-3">
+                        <span className="text-2xl font-bold font-marianne">Fiche Metier:</span>
+                        <div className="cursor-pointer" onClick={onClose}>×</div>
+                    </div>
+                    {children}
+                </div>
             </div>
-        </div>
     </div>)
 }
 
