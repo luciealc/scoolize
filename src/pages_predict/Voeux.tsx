a30398ad01a73ce4b2c77df7c03a4da1b753244a
luciealc <114408320+luciealc@users.noreply.github.com>
2023-12-22 17:45:15 +0100

    page voeux finish

--- src/pages_predict/Voeux.tsx
+++ src/pages_predict/Voeux.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import MainContentPage from './components/MainContent';
+import voeux from "../assets/images/voeux frame.svg"
 
 export interface IVoeuxPageProps {};
 
 const VoeuxPage: React.FunctionComponent<IVoeuxPageProps>=(props)=>{
     return (<div>
         <MainContentPage>
-            <h1>Voeux</h1>
+            <img src={voeux} alt="Predict" className="h-[811px] w-[1114px]" />
         </MainContentPage>
     </div>)
 }
